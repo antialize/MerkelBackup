3011350bd5dcac2f6280f6fe814c794a4f46e2d6
Jakob Truelsen <jakob@scalgo.com>
2019-05-30 23:19:54 +0200

    Improve recovery, validation and more

--- src/client/visit.rs
+++ src/client/visit.rs
@@ -84,11 +84,207 @@ pub enum Mode {
     },
 }
 
-pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
-    let mut client = reqwest::Client::new();
+struct Ent {
+    etype: EType,
+    path: std::path::PathBuf,
+    size: u64,
+    st_mode: u32,
+    uid: u32,
+    gid: u32,
+    atime: i64,
+    mtime: i64,
+    chunks: Vec<String>,
+}
 
+fn row_ent(path: &PathBuf, row: &str, mode: &Mode) -> Result<Option<Ent>, Error> {
+    if row.is_empty() {
+        return Ok(None);
+    }
+
+    let mut ans = row.split('\0');
+    let name = ans.next().ok_or(Error::Msg("Missing name"))?;
+    let etype: EType = ans.next().ok_or(Error::Msg("Missing type"))?.parse()?;
+    let size: u64 = ans.next().ok_or(Error::Msg("Missing size"))?.parse()?;
+    let reference = ans.next().ok_or(Error::Msg("Missing reference"))?;
+    let st_mode: u32 = ans.next().ok_or(Error::Msg("Missing mode"))?.parse()?;
+    let uid: u32 = ans.next().ok_or(Error::Msg("Missing uid"))?.parse()?;
+    let gid: u32 = ans.next().ok_or(Error::Msg("Missing gid"))?.parse()?;
+    let mtime: i64 = ans.next().ok_or(Error::Msg("Missing mtime"))?.parse()?;
+    let atime: i64 = ans.next().ok_or(Error::Msg("Missing atime"))?.parse()?;
+    let _ctime: i64 = ans.next().ok_or(Error::Msg("Missing ctime"))?.parse()?;
+
+    let path = path.join(name);
+    if let Mode::Restore {
+        dry: _,
+        dest: _,
+        pattern,
+        root: _,
+        preserve_owner: _,
+    } = &mode
+    {
+        if !(path.starts_with(pattern) || (pattern.starts_with(&path) && etype == EType::Dir)) {
+            debug!("Skip {:?}", path);
+            return Ok(None);
+        }
+    };
+
+    return Ok(Some(Ent {
+        path,
+        etype,
+        size,
+        st_mode,
+        uid,
+        gid,
+        mtime,
+        atime,
+        chunks: reference.split(",").map(|s| s.to_string()).collect(),
+    }));
+}
+
+fn recover_entry(
+    pb: &mut ProgressBar<std::io::Stdout>,
+    ent: &Ent,
+    dry: bool,
+    dest: &PathBuf,
+    preserve_owner: bool,
+    client: &mut reqwest::Client,
+    config: &Config,
+    secrets: &Secrets,
+) -> Result<(), Error> {
+    pb.message(&format!("{:?}: ", &ent.path));
+    let dpath = dest.join(
+        ent.path
+            .strip_prefix("/")
+            .map_err(|_| Error::Msg("Path not absolute"))?,
+    );
+    match ent.etype {
+        EType::Dir => {
+            debug!("DIR {:?}", dpath);
+            if !dry {
+                std::fs::create_dir_all(&dpath)?;
+            }
+            pb.add(ent.size);
+        }
+        EType::Link => {
+            debug!("LINK {:?}", dpath);
+            if !dry {
+                std::os::unix::fs::symlink(&dpath, ent.chunks.first().unwrap())?;
+            }
+            pb.add(ent.size);
+        }
+        EType::File => {
+            debug!("FILE {:?}", dpath);
+            if !dry {
+                let mut file = std::fs::File::create(&dpath)?;
+                for chunk in ent.chunks.iter() {
+                    let res = get_chunk(client, &config, &secrets, &chunk)?;
+                    file.write(&res)?;
+                    pb.add(res.len() as u64);
+                }
+            } else {
+                pb.add(ent.size);
+            }
+        }
+    }
+    if !dry {
+        std::fs::set_permissions(&dpath, std::fs::Permissions::from_mode(ent.st_mode))?;
+        if preserve_owner {
+            nix::unistd::fchownat(
+                None,
+                &dpath,
+                Some(nix::unistd::Uid::from_raw(ent.uid)),
+                Some(nix::unistd::Gid::from_raw(ent.gid)),
+                nix::unistd::FchownatFlags::NoFollowSymlink,
+            )?;
+        }
+        filetime::set_file_times(
+            &dpath,
+            filetime::FileTime::from_unix_time(ent.atime, 0),
+            filetime::FileTime::from_unix_time(ent.mtime, 0),
+        )?;
+    }
+
+    return Ok(());
+}
+
+pub struct Root<'l> {
+    pub id: u64,
+    pub host: &'l str,
+    pub time: i64,
+    pub hash: &'l str,
+}
+
+pub struct RootsIter<'l> {
+    rows: std::str::Split<'l, &'l str>,
+    filter: Option<&'l str>,
+}
+
+impl<'l> Iterator for RootsIter<'l> {
+    type Item = Result<Root<'l>, Error>;
+
+    fn next(&mut self) -> Option<Result<Root<'l>, Error>> {
+        while let Some(row) = self.rows.next() {
+            if row.is_empty() {
+                continue;
+            }
+            let extract = || -> Result<Root<'l>, Error> {
+                let mut ans = row.split('\0');
+                let id: u64 = ans.next().ok_or(Error::Msg("Missing id"))?.parse()?;
+                let host = ans.next().ok_or(Error::Msg("Missing host"))?;
+                let time: i64 = ans.next().ok_or(Error::Msg("Missing time"))?.parse()?;
+                let hash = ans.next().ok_or(Error::Msg("Missing hash"))?;
+                return Ok(Root {
+                    id,
+                    host,
+                    time,
+                    hash,
+                });
+            };
+            match extract() {
+                Err(e) => return Some(Err(e)),
+                Ok(root) => {
+                    if let Some(filter) = self.filter {
+                        if *filter != format!("{}", root.id)
+                            && *filter
+                                != format!(
+                                    "{} {}",
+                                    root.host,
+                                    NaiveDateTime::from_timestamp(root.time, 0)
+                                )
+                            && *filter != *root.hash
+                        {
+                            continue;
+                        }
+                    }
+                    return Some(Ok(root));
+                }
+            }
+        }
+        return None;
+    }
+}
+
+pub struct Roots<'l> {
+    text: String,
+    filter: Option<&'l str>,
+}
+
+impl<'l> Roots<'l> {
+    pub fn iter(&self) -> RootsIter {
+        RootsIter {
+            rows: self.text.split("\0\0"),
+            filter: self.filter,
+        }
+    }
+}
+
+pub fn roots<'a: 'b, 'b>(
+    config: &Config,
+    secrets: &Secrets,
+    client: &reqwest::Client,
+    filter: Option<&'a str>,
+) -> Result<Roots<'b>, Error> {
     let url = format!("{}/roots/{}", &config.server, hex::encode(&secrets.bucket));
-
     let mut res = check_response(
         client
             .get(&url[..])
@@ -96,20 +292,14 @@ pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
             .send()?,
     )?;
 
-    struct Ent {
-        etype: EType,
-        path: std::path::PathBuf,
-        size: u64,
-        st_mode: u32,
-        uid: u32,
-        gid: u32,
-        atime: i64,
-        mtime: i64,
-        chunks: Vec<String>,
-    }
+    let text = res.text().expect("utf-8");
+    return Ok(Roots { filter, text });
+}
+
+pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
+    let mut client = reqwest::Client::new();
 
     let mut entries: Vec<Ent> = Vec::new();
-
     let mut dirs: HashMap<String, std::path::PathBuf> = HashMap::new();
     let mut dir_stack: Vec<(std::path::PathBuf, String)> = Vec::new();
     let mut bad_dirs: usize = 0;
@@ -120,47 +310,38 @@ pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
 
     let mut root_found = false;
 
-    for row in res.text().expect("utf-8").split("\0\0") {
-        if row.is_empty() || root_found {
-            continue;
+    let root: Option<&str> = {
+        if let Mode::Restore {
+            dry: _,
+            dest: _,
+            pattern: _,
+            root,
+            preserve_owner: _,
+        } = &mode
+        {
+            Some(root)
+        } else {
+            None
         }
-        let ans: Vec<&str> = row.split('\0').collect();
-        let id = ans.get(0).ok_or(Error::MissingRow())?.parse::<u64>()?;
-        let host = ans.get(1).expect("Missing host");
-        let time = ans.get(2).ok_or(Error::MissingRow())?.parse::<i64>()?;
-        let hash = ans.get(3).ok_or(Error::MissingRow())?;
-        dir_stack.push((std::path::PathBuf::new(), hash.to_string()));
+    };
 
-        match &mode {
-            Mode::Validate { full: _ } => (),
-            Mode::Restore {
-                dry: _,
-                dest: _,
-                pattern: _,
-                root,
-                preserve_owner: _,
-            } => {
-                if *root != format!("{}", id)
-                    && *root != format!("{} {}", host, NaiveDateTime::from_timestamp(time, 0))
-                    && root != hash
-                {
-                    continue;
-                }
-                root_found = true;
-            }
-            Mode::Prune { dry } => {
-                if time + 60 * 60 * 60 * 60 * 24 * 90 < now {
+    for root in roots(&config, &secrets, &client, root)?.iter() {
+        let root = root?;
+        root_found = true;
+        if let Mode::Prune { dry, age } = &mode {
+            if let Some(age) = age {
+                if root.time + 60 * 60 * 24 * (*age as i64) < now {
                     info!(
                         "Removing root {} {}",
-                        host,
-                        NaiveDateTime::from_timestamp(time, 0)
+                        root.host,
+                        NaiveDateTime::from_timestamp(root.time, 0)
                     );
                     if !dry {
                         let url = format!(
                             "{}/roots/{}/{}",
                             &config.server,
                             hex::encode(&secrets.bucket),
-                            id
+                            root.id
                         );
                         check_response(
                             client
@@ -172,11 +353,12 @@ pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
                     continue;
                 }
             }
-        };
+        }
+        dir_stack.push((std::path::PathBuf::new(), root.hash.to_string()));
         info!(
             "Visiting root {} {}",
-            host,
-            NaiveDateTime::from_timestamp(time, 0)
+            root.host,
+            NaiveDateTime::from_timestamp(root.time, 0)
         );
         while let Some((path, hash)) = dir_stack.pop() {
             match dirs.entry(hash.clone()) {
@@ -196,71 +378,34 @@ pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
             };
 
             for row in v.split("\0\0") {
-                if row.is_empty() {
-                    continue;
-                }
-                if let Err(e) = (|| -> Result<(), Error> {
-                    let mut ans = row.split('\0');
-                    let name = ans.next().ok_or(Error::Msg("Missing name"))?;
-                    let etype: EType = ans.next().ok_or(Error::Msg("Missing type"))?.parse()?;
-                    let size: u64 = ans.next().ok_or(Error::Msg("Missing size"))?.parse()?;
-                    let reference = ans.next().ok_or(Error::Msg("Missing reference"))?;
-                    let st_mode: u32 = ans.next().ok_or(Error::Msg("Missing mode"))?.parse()?;
-                    let uid: u32 = ans.next().ok_or(Error::Msg("Missing uid"))?.parse()?;
-                    let gid: u32 = ans.next().ok_or(Error::Msg("Missing gid"))?.parse()?;
-                    let mtime: i64 = ans.next().ok_or(Error::Msg("Missing mtime"))?.parse()?;
-                    let atime: i64 = ans.next().ok_or(Error::Msg("Missing atime"))?.parse()?;
-                    let _ctime: i64 = ans.next().ok_or(Error::Msg("Missing ctime"))?.parse()?;
-
-                    let path = path.join(name);
-                    if let Mode::Restore {
-                        dry: _,
-                        dest: _,
-                        pattern,
-                        root: _,
-                        preserve_owner: _,
-                    } = &mode
-                    {
-                        if !(path.starts_with(pattern)
-                            || (pattern.starts_with(&path) && etype != EType::Dir))
-                        {
-                            return Ok(());
+                match row_ent(&path, row, &mode) {
+                    Ok(None) => {}
+                    Ok(Some(ent)) => {
+                        if ent.etype == EType::Dir {
+                            dir_stack
+                                .push((ent.path.clone(), ent.chunks.first().unwrap().to_string()));
                         }
-                    };
-
-                    if etype == EType::Dir {
-                        dir_stack.push((path.clone(), reference.to_string()));
+                        entries.push(ent);
                     }
-
-                    entries.push(Ent {
-                        path,
-                        etype,
-                        size,
-                        st_mode,
-                        uid,
-                        gid,
-                        mtime,
-                        atime,
-                        chunks: reference.split(",").map(|s| s.to_string()).collect(),
-                    });
-
-                    return Ok(());
-                })() {
-                    bad_dirs += 1;
-                    error!(
-                        "Bad row '{}` in dir {} at path {:?}: {:?}",
-                        row, hash, path, e
-                    );
+                    Err(e) => {
+                        bad_dirs += 1;
+                        error!(
+                            "Bad row '{}` in dir {} at path {:?}: {:?}",
+                            row, hash, path, e
+                        );
+                    }
                 }
             }
         }
     }
 
-    error!("{} of {} dirs are bad", bad_dirs, dirs.len());
+    if bad_dirs != 0 {
+        error!("{} of {} dirs are bad", bad_dirs, dirs.len());
+    }
 
-    match mode {
+    match &mode {
         Mode::Validate { full } => {
-            if full {
+            if *full {
                 let mut files: HashMap<&str, (usize, &PathBuf)> = HashMap::new();
                 let mut bytes: u64 = 0;
                 for ent in entries.iter() {
@@ -383,57 +528,18 @@ pub fn run(config: Config, secrets: Secrets, mode: Mode) -> Result<(), Error> {
             pb.set_max_refresh_rate(Some(Duration::from_millis(500)));
             pb.set_units(pbr::Units::Bytes);
             for ent in entries {
-                pb.message(&format!("{:?}: ", &ent.path));
-                let dpath = dest.join(
-                    ent.path
-                        .strip_prefix("/")
-                        .map_err(|_| Error::Msg("Path not absolute"))?,
-                );
-                match ent.etype {
-                    EType::Dir => {
-                        info!("DIR {:?}", dpath);
-                        if !dry {
-                            std::fs::create_dir_all(&dpath)?;
-                        }
-                        pb.add(ent.size);
-                    }
-                    EType::Link => {
-                        info!("LINK {:?}", dpath);
-                        if !dry {
-                            std::os::unix::fs::symlink(&dpath, ent.chunks.first().unwrap())?;
-                        }
-                        pb.add(ent.size);
-                    }
-                    EType::File => {
-                        info!("FILE {:?}", dpath);
-                        if !dry {
-                            let mut file = std::fs::File::create(&dpath)?;
-                            for chunk in ent.chunks {
-                                let res = get_chunk(&mut client, &config, &secrets, &chunk)?;
-                                file.write(&res)?;
-                                pb.add(res.len() as u64);
-                            }
-                        } else {
-                            pb.add(ent.size);
-                        }
-                    }
-                }
-                if !dry {
-                    std::fs::set_permissions(&dpath, std::fs::Permissions::from_mode(ent.st_mode))?;
-                    if preserve_owner {
-                        nix::unistd::fchownat(
-                            None,
-                            &dpath,
-                            Some(nix::unistd::Uid::from_raw(ent.uid)),
-                            Some(nix::unistd::Gid::from_raw(ent.gid)),
-                            nix::unistd::FchownatFlags::NoFollowSymlink,
-                        )?;
-                    }
-                    filetime::set_file_times(
-                        &dpath,
-                        filetime::FileTime::from_unix_time(ent.atime, 0),
-                        filetime::FileTime::from_unix_time(ent.mtime, 0),
-                    )?;
+                if let Err(e) = recover_entry(
+                    &mut pb,
+                    &ent,
+                    *dry,
+                    &dest,
+                    *preserve_owner,
+                    &mut client,
+                    &config,
+                    &secrets,
+                ) {
+                    error!("Unable to recover entry {:?}: {:?}", ent.path, e);
+                    return Err(e);
                 }
             }
         }
